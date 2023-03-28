3c6454bea9176caa7ca94adec9b7f2c6870bc0ef
aabdousouid <abdelwehedsouid@gmail.com>
2023-03-28 01:01:23 +0100

    test 8

--- src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
-public class ProduitServiceImplMockTest {
+public class ProduitServiceImplTest {
 
     @Mock
     ProduitRepository produitRepository;
