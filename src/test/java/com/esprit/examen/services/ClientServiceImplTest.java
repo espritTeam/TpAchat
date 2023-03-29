793e9c0179b178d1e95bf0c3d5b6681170127800
aabdousouid <74819800+aabdousouid@users.noreply.github.com>
2023-03-29 22:19:30 +0100

    Update ClientServiceImplTest.java

--- src/test/java/com/esprit/examen/services/ClientServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/ClientServiceImplTest.java
@@ -33,7 +33,7 @@ public class ClientServiceImplTest {
 
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		Date dateNaissance = dateFormat.parse("30/09/2000");
-		Client c = new Client("Salhi", "Ahmed", dateNaissance, "ahmed.salhi@esprit.tn", "pwd", Profession.Cadre,
+		Client c = new Client("Salhi", "Ahmed", dateNaissance , "ahmed.salhi@esprit.tn", "pwd", Profession.Cadre,
 				CategorieClient.Ordinaire);
 		Client client = clientService.addClient(c);
 		System.out.print("client "+client);
