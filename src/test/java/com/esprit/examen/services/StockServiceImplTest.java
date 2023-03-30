46c60697736ac38394b0054ec7e3c4ffc8ea0983
aabdousouid <74819800+aabdousouid@users.noreply.github.com>
2023-03-30 12:18:24 +0100

    Update StockServiceImplTest.java

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -31,7 +31,7 @@ public class StockServiceImplTest {
 	@Test
 	public void testAddStockOptimized() {
 
-		Stock s = new Stock("stock test",10,100 );
+		Stock s = new Stock("stock test",10,100);
 		Stock savedStock= stockService.addStock(s);
 		assertNotNull(savedStock.getIdStock());
 		assertSame(10, savedStock.getQte());
