c90e9489f6f90fcc6cf19050d9192c98fa2bdf15
aabdousouid <abdelwehedsouid@gmail.com>
2023-03-30 22:15:57 +0100

    sonar error build fix 2

--- src/main/java/com/esprit/examen/repositories/ProduitRepository.java
+++ src/main/java/com/esprit/examen/repositories/ProduitRepository.java
@@ -10,9 +10,20 @@ import com.esprit.examen.entities.Produit;
 
 @Repository
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
+
 	
-	@Query("SELECT sum(df.prixTotalDetail) FROM DetailFacture df where df.produit=:produit and df.facture.dateCreationFacture between :startDate"
-			+ " and :endDate and df.facture.archivee=true")
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*@Query("SELECT sum(df.prixTotal) FROM DetailFacture df where df.produit=:produit and df.facture.dateFacture between :startDate"
+			+ " and :endDate and df.facture.active=true")
 	public float getRevenuBrutProduit(@Param("produit") Produit produit, @Param("startDate") Date startDate,
-			@Param("endDate") Date endDate);
+			@Param("endDate") Date endDate);*/
 }
