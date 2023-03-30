199ab0f3a025ef9666ffad5b8bf69dd67597be65
Boubakermrabet <88195728+Boubakermrabet@users.noreply.github.com>
2023-03-30 17:39:18 +0200

    Update StockServiceImplTest.java

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -23,10 +23,12 @@ public class StockServiceImplTest {
 	//	List<Stock> stocks = stockService.retrieveAllStocks();
 	//	int expected=stocks.size();
 		Stock s = new Stock("stock test",10,100);
+		
 		Stock savedStock= stockService.addStock(s);
 		
 	//	assertEquals(expected+1, stockService.retrieveAllStocks().size());
 		assertNotNull(savedStock.getLibelleStock());
+		
 		stockService.deleteStock(savedStock.getIdStock());
 		
 	} 
