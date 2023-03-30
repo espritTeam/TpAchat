1226c39b2351c7c196c97176a07ce28ef15dd6ff
arfaouicyrine <75760878+arfaouicyrine@users.noreply.github.com>
2023-03-30 14:42:42 +0100

    Update StockServiceImplTest.java

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -17,6 +17,7 @@ public class StockServiceImplTest {
 	
 	@Test
 	public void testAddStock() {
+	
 	//	List<Stock> stocks = stockService.retrieveAllStocks();
 	//	int expected=stocks.size();
 		Stock s = new Stock("stock test",10,100);
