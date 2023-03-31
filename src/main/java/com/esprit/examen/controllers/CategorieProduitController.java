9a6d5195b1b8b1fdc636d74301e28859de94edd7
Rama945 <100422515+Rama945@users.noreply.github.com>
2023-03-31 09:52:47 +0100

    Update CategorieProduitController.java

--- src/main/java/com/esprit/examen/controllers/CategorieProduitController.java
+++ src/main/java/com/esprit/examen/controllers/CategorieProduitController.java
@@ -24,7 +24,7 @@ public class CategorieProduitController {
 	@Autowired
 	ICategorieProduitService categorieProduitService;
 	
-	// http://localhost:8089/SpringMVC/categorieProduit/retrieve-all-categorieProduit ---
+	// http://localhost:8089/SpringMVC/categorieProduit/retrieve-all-categorieProduit --- ---
 	@GetMapping("/retrieve-all-categorieProduit")
 	@ResponseBody
 	public List<CategorieProduit> getCategorieProduit() {
